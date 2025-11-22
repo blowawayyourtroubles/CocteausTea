b9ca45be25a9dc8b978f3732bc4301aef9eb68f1
German Gibbs <84513918+blowawayyourtroubles@users.noreply.github.com>
2025-11-22 15:48:59 -0400

    changes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-
 const barras = document.getElementById('barras');
 const navMenu = document.querySelector('.navbar-list');   
-const body = document.body;                         
+const body = document.body;
+const navLinks = document.querySelectorAll('.nav-item'); 
 
-
+// Toggle Menu
 barras.addEventListener('click', () => {
-  
   barras.classList.toggle('fa-bars');
-  barras.classList.toggle('fa-xmark');
+  barras.classList.toggle('fa-xmark'); // Switches icon to 'X'
   navMenu.classList.toggle('active');
   body.classList.toggle('no-scroll');
 });
 
-
+// Close menu when a link is clicked
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    barras.classList.add('fa-bars');
+    barras.classList.remove('fa-xmark');
+    navMenu.classList.remove('active');
+    body.classList.remove('no-scroll');
+  });
+});
